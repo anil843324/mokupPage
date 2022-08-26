28a0561df8b959f66862eb6c9033107feae7d3c6
anil843324 <anilkrsingh14200@gmail.com>
2022-08-26 09:36:46 +0530

    now data are fatched

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useSelector ,useDispatch } from 'react-redux';
+import {dataList} from "../redux/dataAction"
 const Home = () => {
+    const dispatch=useDispatch()
+  const data = useSelector((state) => state.fetchData);
+   
 
+
+  useEffect(() => {
+    dispatch(dataList());
+  }, []);
+
+   console.log(data)
+
+  return (
+    <div>
 
     
-  return (
-    <div>Home</div>
+
+
+
+
+    </div>
   )
 }
 
