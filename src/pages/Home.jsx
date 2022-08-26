d6552a25b9b130e0c0934cd056c79cac50308f91
anil843324 <anilkrsingh14200@gmail.com>
2022-08-26 11:07:40 +0530

    home page ui done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,36 @@
 import React, { useEffect } from 'react'
-import { useSelector ,useDispatch } from 'react-redux';
-import {dataList} from "../redux/dataAction"
+import { useSelector, useDispatch } from 'react-redux';
+import { dataList } from "../redux/dataAction"
+import "../pages/Home.css"
 const Home = () => {
-    const dispatch=useDispatch()
+  const dispatch = useDispatch()
   const data = useSelector((state) => state.fetchData);
-   
+
 
 
   useEffect(() => {
     dispatch(dataList());
   }, []);
 
-   console.log(data)
+  console.log(data)
 
   return (
-    <div>
+    <div className='homeContainer'>
+         <hr />
+      <span className='highlight'>Highlights</span>
+       
+      <div className='dataBigConatainer'>
+        {
+          data.map((ele) => (
+            <div className='dataContainer'>
+                
+                 <img src={ele.url} alt="pic" />
 
-    
+            </div>
+          ))
+
+        }
+      </div>
 
 
 
